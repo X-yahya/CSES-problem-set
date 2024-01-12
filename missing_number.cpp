6b8cd1dc106c0e4be39ae583b61498fc8531915e
lyt <124516380+X-yahya@users.noreply.github.com>
2024-01-12 23:56:17 +0100

    update

--- missing_number.cpp
+++ missing_number.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-using namespace std ; 
-int main()
-{
-    
+using namespace std;
+
+int main() {
+    long int n, expsum = 0, actsum = 0;
+
+    do {
+        cin >> n;
+    } while (n <= 0);
+
+    for (int i = 1; i <= n; ++i) {
+        expsum += i;
+    }
+
+    long int t[n - 1];
+    for (int i = 0; i < n - 1; ++i) {
+        cin >> t[i];
+        actsum += t[i];
+    }
+
+    long int missingNumber = expsum - actsum;
+    cout << missingNumber << endl;
+
     return 0;
 }
